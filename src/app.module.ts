9b2e855cac3702aa13c900c32bcebb118ebeafee
Andre Albuquerque <albuquerque-andre@ibm.com>
2023-08-21 15:54:29 -0300

    wip imersao 14

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { RoutesModule } from './routes/routes.module';
+
+
+
+@Module({
+  imports: [PrismaModule, RoutesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
